bab97e41d0c75178329cc447fc1030cac0b150d0
merveille <merveillezebaze8@gmail.com>
2022-07-20 17:28:27 +0100

    'push'

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Order Delivery Status",
+    'summary': """Delivery Status on Sale Order""",
+    'description': """This module adds Delivery Status on Sale Order""",
+
+    'author': "Merveille ZEBAZE",
+    'maintainer': 'Merveille ZEBAZE',
+    'website': "http://merveillezebaze.pythonanywhere.com/",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['sale_stock', 'sale_management'],
+
+    'data': ['views/sales_order.xml'],
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
